5737fd5c1c96925245693b6232c7756df25b7cea
Leonardo Diniz <137980570+lpndev@users.noreply.github.com>
2024-08-10 22:26:02 -0700

    fix: organizing packages order

--- scripts/winget-packages.ps1
+++ scripts/winget-packages.ps1
@@ -3,7 +3,7 @@ if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
     exit 1
 }
 
-$commands = @(
+$compatibility = @(
     "winget install Microsoft.DirectX",
     "winget install Microsoft.XNARedist",
     "winget install EclipseAdoptium.Temurin.21.JRE",
@@ -11,11 +11,48 @@ $commands = @(
     "winget install EclipseAdoptium.Temurin.8.JRE"
 )
 
-foreach ($command in $commands) {
-    try {
-        Invoke-Expression $command
-    }
-    catch {
-        Write-Host "Error executing: $command`nError: $_"
+$tools = @(
+    "winget install Anki.Anki",
+    "winget install CPUID.HWMonitor",
+    "winget install DevToys-app.DevToys",
+    "winget install Klocman.BulkCrapUninstaller",
+    "winget install Microsoft.VisualStudioCode",
+    "winget install MHNexus.HxD",
+    "winget install PuTTY.PuTTY",
+    "winget install RARLab.WinRAR",
+    "winget install voidtools.Everything",
+    "winget install WinSCP.WinSCP"
+)
+
+$applications = @(
+    "winget install Audacity.Audacity",
+    "winget install Bitwarden.Bitwarden",
+    "winget install Brave.Brave",
+    "winget install Discord.Discord",
+    "winget install Docker.DockerDesktop",
+    "winget install Flow-Launcher.Flow-Launcher",
+    "winget install HeroicGamesLauncher.HeroicGamesLauncher",
+    "winget install Obsidian.Obsidian",
+    "winget install PrismLauncher.PrismLauncher",
+    "winget install ShareX.ShareX",
+    "winget install Spotify.Spotify",
+    "winget install Valve.Steam",
+    "winget install VideoLAN.VLC"
+)
+
+$compatibility = $compatibility | Sort-Object
+$tools = $tools | Sort-Object
+$applications = $applications | Sort-Object
+
+$categories = @($compatibility, $tools, $applications)
+foreach ($category in $categories) {
+    foreach ($command in $category) {
+        try {
+            Write-Host "Executing: $command"
+            Invoke-Expression $command
+        }
+        catch {
+            Write-Host "Error executing: $command`nError: $_"
+        }
     }
 }
