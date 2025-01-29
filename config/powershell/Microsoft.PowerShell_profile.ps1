64d02f3355731064d49df791759d64bebc521bf6
Leonardo Diniz <137980570+lpndev@users.noreply.github.com>
2025-01-29 15:40:36 -0800

    🐛 fix: automatically identified user for komorebi in powershell, and removed hide taskbar in yasb

--- config/powershell/Microsoft.PowerShell_profile.ps1
+++ config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,4 @@
-$Env:KOMOREBI_CONFIG_HOME = 'C:\Users\lain\.config\komorebi'
+$Env:KOMOREBI_CONFIG_HOME = 'C:\Users\$env:USERNAME\.config\komorebi'
 
 Import-Module -Name Terminal-Icons
 Invoke-Expression (&starship init powershell)
