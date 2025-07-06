76d44fe1fa2542a4b64527f791e4077ca90536b8
lpndev <137980570+lpndev@users.noreply.github.com>
2025-07-05 17:20:26 -0700

    🔧 chore: set fish_greeting to fastfetch

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,7 +2,9 @@ if test -z "$DISPLAY"; and test "$XDG_VTNR" = "1"
     exec Hyprland
 end
 
-set fish_greeting
+function fish_greeting
+    fastfetch
+end
 
 if test -f ~/.config/fish/aliases.fish
     source ~/.config/fish/aliases.fish
