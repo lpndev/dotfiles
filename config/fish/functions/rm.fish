119e023d6bbb4019602009ba597985ff2b3be506
Leonardo Diniz <137980570+lpndev@users.noreply.github.com>
2025-07-14 16:41:03 -0700

    📝 update: aliases from abbr to alias and enhance update function with confirmation prompts

--- config/fish/functions/rm.fish
+++
@@ -1,3 +0,0 @@
-function rm
-    command rm -ifrv $argv
-end
--- config/fish/functions/update.fish
+++ config/fish/functions/update.fish
@@ -1,3 +1,25 @@
 function update
-  yay -Syu --noconfirm;
+    set -l stamp ~/.last_update
+
+    # Check if update was run in the last 7 days
+    if test -f $stamp
+        set -l last (math (date +%s) - (date -r $stamp +%s))
+        if test $last -lt 604800
+            echo "You already updated in the last 7 days."
+            read -l -P "Proceed anyway? [y/N] " confirm
+            if test "$confirm" != "y"
+                echo "Update cancelled."
+                return
+            end
+        end
+    end
+
+    # Confirmation prompt
+    read -l -P "Are you sure you want to update? [y/N] " confirm
+    if test "$confirm" = "y"
+        yay -Syu --noconfirm
+        touch $stamp
+    else
+        echo "Update cancelled."
+    end
 end
