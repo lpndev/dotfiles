d0905e9dc9bde0ebe422f004f4552b0d9187dd6d
Leonardo Diniz <137980570+lpndev@users.noreply.github.com>
2025-08-13 22:50:28 -0300

    🔧 chore: include default theme source in fish configuration

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,6 +1,7 @@
 function fish_greeting; fastfetch; end
 
 source ~/.config/fish/aliases.fish
+source ~/.config/fish/themes/default.theme
 
 set --export STARSHIP_CONFIG ~/.config/starship/starship.toml
 starship init fish | source
