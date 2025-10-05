436aa93c7640c9fb1bc0a66e8413611458049b3e
Leonardo Diniz <137980570+lpndev@users.noreply.github.com>
2025-10-04 23:01:46 -0300

    feat: update dotfiles and changed preview

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,10 @@
 function fish_greeting; fastfetch; end
 
 source ~/.config/fish/aliases.fish
-source ~/.config/fish/themes/default.theme
 
 set --export STARSHIP_CONFIG ~/.config/starship/starship.toml
+
 starship init fish | source
 fzf --fish | source
 zoxide init fish | source
+~/.local/bin/mise activate fish | source
